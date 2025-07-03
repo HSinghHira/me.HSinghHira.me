f884b805ce931bf8a97efd44ffa0389f4849bf64
Harman Singh Hira <HSinghHira@users.noreply.github.com>
2025-07-03 17:54:54 +1200

    Building

--- .vitepress/data/NavLinks.ts
+++ .vitepress/data/NavLinks.ts
@@ -2,31 +2,31 @@ export const menuItems = [
   {
     label: 'Introduction',
     icon: 'ri-user-line',
-    url: '/'
+    url: 'https://git.hsinghhira.me/me.HSinghHira.me/'
   },
   {
     label: 'Pro Work Experience',
     icon: 'fa-briefcase',
-    url: '/pages/work/'
+    url: 'https://git.hsinghhira.me/me.HSinghHira.me/pages/work/'
   },
   {
     label: 'Learning & Education',
     icon: 'fa-graduation-cap',
-    url: '/pages/edu/'
+    url: 'https://git.hsinghhira.me/me.HSinghHira.me/pages/edu/'
   },
   {
     label: 'Projects',
     icon: 'fa-folder-open',
-    url: '/pages/projects/'
+    url: 'https://git.hsinghhira.me/me.HSinghHira.me/pages/projects/'
   },
   {
     label: 'Blog',
     icon: 'fa-edit',
-    url: '/pages/blog/'
+    url: 'https://git.hsinghhira.me/me.HSinghHira.me/pages/blog/'
   },
   {
     label: 'Contact',
     icon: 'fa-envelope',
-    url: '/pages/contact/'
+    url: 'https://git.hsinghhira.me/me.HSinghHira.me/pages/contact/'
   }
 ]
