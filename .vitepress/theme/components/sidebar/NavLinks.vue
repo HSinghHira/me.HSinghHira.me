8e2b8d5097d26874ca85f4258b4d1343c73e66b7
Harman Singh Hira <HSinghHira@users.noreply.github.com>
2025-07-03 18:10:38 +1200

    Building

--- .vitepress/theme/components/sidebar/NavLinks.vue
+++ .vitepress/theme/components/sidebar/NavLinks.vue
@@ -117,7 +117,40 @@
 <script setup>
 import { ref, watch } from 'vue';
 import { useRouter } from 'vitepress';
-import { menuItems } from '../../../data/NavLinks';
+
+// Menu items defined directly in the component
+const menuItems = [
+  {
+    label: 'Introduction',
+    icon: 'ri-user-line',
+    url: '/'
+  },
+  {
+    label: 'Pro Work Experience',
+    icon: 'fa-briefcase',
+    url: '/pages/work/'
+  },
+  {
+    label: 'Learning & Education',
+    icon: 'fa-graduation-cap',
+    url: 'pages/edu/'
+  },
+  {
+    label: 'Projects',
+    icon: 'fa-folder-open',
+    url: '/pages/projects/'
+  },
+  {
+    label: 'Blog',
+    icon: 'fa-edit',
+    url: '/pages/blog/'
+  },
+  {
+    label: 'Contact',
+    icon: 'fa-envelope',
+    url: '/pages/contact/'
+  }
+];
 
 const props = defineProps({
   isDesktop: Boolean
